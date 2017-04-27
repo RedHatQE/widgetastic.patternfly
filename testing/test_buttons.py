82b582a2e5dfc2d7b6fa6e37cc15eaf8fe8db632
Jan Kročil <jkrocil@redhat.com>
2017-04-27 17:55:34 +0200

    Add test

--- testing/test_buttons.py
+++ testing/test_buttons.py
@@ -5,9 +5,11 @@ from widgetastic_patternfly import Button
 
 def test_button_click(browser):
     class TestView(View):
+        any_button = Button()
         button1 = Button('Default Normal')
         button2 = Button(title='Destructive title')
 
     view = TestView(browser)
+    assert view.any_button.is_displayed
     assert view.button1.is_displayed
     assert view.button2.is_displayed
