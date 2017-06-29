e4b6f12b869682468e7ab23eca776ddaea630cad
Mike Shriver <mshriver@redhat.com>
2017-06-29 11:04:31 -0400

    Update tests for Button with classes+attributes

--- testing/test_buttons.py
+++ testing/test_buttons.py
@@ -8,8 +8,10 @@ def test_button_click(browser):
         any_button = Button()
         button1 = Button('Default Normal')
         button2 = Button(title='Destructive title')
+        button3 = Button(title='noText', classes=[Button.PRIMARY])
 
     view = TestView(browser)
     assert view.any_button.is_displayed
     assert view.button1.is_displayed
     assert view.button2.is_displayed
+    assert view.button3.is_displayed
