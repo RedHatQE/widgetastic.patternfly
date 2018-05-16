a3d6371d451b2aa3fd47f06b8409569fc97c1cd3
Mike Shriver <mshriver@redhat.com>
2018-05-16 10:47:43 -0400

    Add breadcrumb widget
    copypasta from ManageIQ/integration_tests/widgetastic_manageiq

--- src/widgetastic_patternfly/__init__.py
+++ src/widgetastic_patternfly/__init__.py
@@ -1804,3 +1804,48 @@ class AboutModal(Widget):
             # value will include the label from the <strong> block, parse it out
             items.update({key: element_text.replace(key, '', 1).lstrip()})
         return items
+
+
+class BreadCrumb(Widget):
+    """ Patternfly BreadCrumb navigation control
+
+    .. code-block:: python
+
+        breadcrumb = BreadCrumb()
+        breadcrumb.click_location(breadcrumb.locations[0])
+    """
+    ROOT = '//ol[contains(@class, "breadcrumb")]'
+    ELEMENTS = './/li'
+
+    def __init__(self, parent, locator=None, logger=None):
+        Widget.__init__(self, parent=parent, logger=logger)
+        self._locator = locator or self.ROOT
+
+    def __locator__(self):
+        return self._locator
+
+    @property
+    def _path_elements(self):
+        return self.browser.elements(self.ELEMENTS, parent=self)
+
+    @property
+    def locations(self):
+        return [self.browser.text(loc) for loc in self._path_elements]
+
+    @property
+    def active_location(self):
+        br = self.browser
+        return next(br.text(loc) for loc in self._path_elements if 'active' in br.classes(loc))
+
+    def click_location(self, name, handle_alert=True):
+        br = self.browser
+        location = next(loc for loc in self._path_elements if br.text(loc) == name)
+        result = br.click(location, ignore_ajax=handle_alert)
+        if handle_alert:
+            self.browser.handle_alert(wait=2.0, squash=True)
+            self.browser.plugin.ensure_page_safe()
+        return result
+
+    def read(self):
+        """Return the active location of the breadcrumb"""
+        return self.active_location
