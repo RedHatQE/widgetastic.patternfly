995d7646303c6dd6201be0033498ab8ef4b35261
Mike Shriver <mshriver@redhat.com>
2018-09-18 10:57:13 -0400

    Add utils.PFIcon, use firefox for testing
    Change to firefox in conftest
    Add requirements
    Update travis config to include firefox latest-esr
    Update travis config with geckodriver download and requirements install
    Update tox config, travis section deprecation

---
+++ src/widgetastic_patternfly/utils.py
@@ -0,0 +1,62 @@
+# module for patternfly utility classes and methods
+from aenum import Constant
+
+
+class IconConstants(Constant):
+    """class to hold just the icon constants
+
+    References:
+        https://www.patternfly.org/styles/icons/
+    """
+    ADD = 'pficon-add-circle-o'
+    APPLICATIONS = 'pficon-applications'
+    ARROW = 'pficon-arrow'
+    ERROR = 'pficon-error-circle-o'
+    OK = 'pficon-ok'
+    WARNING = 'pficon-warning-triangle-o'
+    REFRESH = 'fa-refresh'
+
+    @classmethod
+    def icon_strings(cls):
+        return {a: s for a, s in vars(IconConstants).items() if isinstance(s, Constant)}
+
+
+class PFIcon(object):
+    """Class to enumerate the patternfly default icons
+
+    pficon-* markup classes have a variety of strings, this class should serve to prevent widget
+    classes from having to parse, pass, or depend on string fragments
+    """
+
+    icons = IconConstants
+
+    @classmethod
+    def icon_from_element(cls, element, browser):
+        """Taking a webelement, scan its child element classes for pficon and fa, return icon state
+
+        Args:
+            element: webelement object that will be searched for pficon classes
+            browser: browser instance to query child elements and classes
+
+        Raises:
+            widgetastic.exceptions.NoSuchElementException when no icon span found
+        """
+
+        els = browser.elements(
+            './/*[contains(@class, "pficon") or contains(@class, "fa")]',
+            parent=element
+        )
+        if len(els) != 1:
+            return None  # multiple icons
+
+        icon_class = [c
+                      for c in browser.classes(els.pop())
+                      if c.startswith('pficon-') or c.startswith('fa-')]
+        # slice off first 6 chars if a class was found
+        icon_name = icon_class or None
+        icons = [
+            getattr(cls.icons, attr, None)
+            for attr, icon_string in cls.icons.icon_strings().items()
+            if icon_string == icon_name
+        ]
+        return icons.pop() if icons else None
--- testing/conftest.py
+++ testing/conftest.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import allure
-import pytest
-
 import codecs
 import os
 import sys
 
+import allure
+import pytest
 from selenium import webdriver
-
 from widgetastic.browser import Browser
 
 selenium_browser = None
@@ -22,7 +20,9 @@ class CustomBrowser(Browser):
 
 @pytest.fixture(scope='session')
 def selenium(request):
-    driver = webdriver.PhantomJS()
+    options = webdriver.FirefoxOptions()
+    options.add_argument('-headless')
+    driver = webdriver.Firefox(firefox_options=options)
     request.addfinalizer(driver.quit)
     driver.maximize_window()
     global selenium_browser
@@ -45,5 +45,12 @@ def browser(selenium, httpserver, request):
 def pytest_exception_interact(node, call, report):
     if selenium_browser is not None:
         allure.attach(
-            'Error screenshot', selenium_browser.get_screenshot_as_png(), allure.attach_type.PNG)
-        allure.attach('Error traceback', str(report.longrepr), allure.attach_type.TEXT)
+            'Error screenshot',
+            selenium_browser.get_screenshot_as_png(),
+            allure.attachment_type.PNG
+        )
+        allure.attach(
+            'Error traceback',
+            str(report.longrepr),
+            allure.attachment_type.TEXT
+        )
