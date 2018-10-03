2673b7cb4d447fff18d573ffd4e07a0f51d9d951
Mike Shriver <mshriver@redhat.com>
2018-10-03 08:15:44 -0400

    Revert travs/tox/requirements changes

--- testing/conftest.py
+++ testing/conftest.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import allure
+import pytest
+
 import codecs
 import os
 import sys
 
-import allure
-import pytest
 from selenium import webdriver
+
 from widgetastic.browser import Browser
 
 selenium_browser = None
@@ -20,9 +22,7 @@ class CustomBrowser(Browser):
 
 @pytest.fixture(scope='session')
 def selenium(request):
-    options = webdriver.FirefoxOptions()
-    options.add_argument('-headless')
-    driver = webdriver.Firefox(firefox_options=options)
+    driver = webdriver.PhantomJS()
     request.addfinalizer(driver.quit)
     driver.maximize_window()
     global selenium_browser
@@ -45,12 +45,5 @@ def browser(selenium, httpserver, request):
 def pytest_exception_interact(node, call, report):
     if selenium_browser is not None:
         allure.attach(
-            'Error screenshot',
-            selenium_browser.get_screenshot_as_png(),
-            allure.attachment_type.PNG
-        )
-        allure.attach(
-            'Error traceback',
-            str(report.longrepr),
-            allure.attachment_type.TEXT
-        )
+            'Error screenshot', selenium_browser.get_screenshot_as_png(), allure.attach_type.PNG)
+        allure.attach('Error traceback', str(report.longrepr), allure.attach_type.TEXT)
