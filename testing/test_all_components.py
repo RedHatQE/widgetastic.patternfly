b5ca7ae731059e20872c25813979acb52677e0eb
Jaroslav Henner <jhenner@redhat.com>
2020-06-23 22:26:19 +0200

    Remove camel_to_snake and use attributize_string

--- testing/test_all_components.py
+++ testing/test_all_components.py
@@ -1,15 +1,15 @@
 """
 This module contains code for auto-discovering Widget classes, instantiating
 them in a programatically defined view class as a new type, which is then
-instantiated and then testing behavior of the widget objects that appear
-to get instantiated upon access.
+instantiated. The widgets appear to get instantiated upon accessing them in the instantiated view.
+After all of this, the behavior of the widget objects can be tested.
 """
 
+from widgetastic.utils import attributize_string
 from widgetastic.widget import View, Widget
 import widgetastic_patternfly as wp
 import inspect
 import pytest
-import re
 
 
 collected_widgets = {
@@ -27,14 +27,8 @@ DUMMY_LOCATOR = "/dummy"
 DUMMY_TREE_ID = "SOME_DUMMY_TREE_ID"
 
 
-def camel_to_snake(s):
-    """ Converts camel-cased string to snake form (undersored lowercase)
-    Taken from https://stackoverflow.com/a/1176023/1950100 """
-    return re.sub(r'(?<!^)(?=[A-Z])', '_', s).lower()
-
-
 # TODO generate from args spec that should by added to definition of the types.
-# Currently some parameters that are enough to sucessfully contruct the
+# Currently some parameters that are enough to successfully construct the
 # instance from the type are chosen.
 init_values = {
     wp.AggregateStatusCard: dict(name=DUMMY_NAME),
@@ -68,7 +62,7 @@ def test_view_class():
     and assigned to it. """
 
     # Instantiate objects to be set in the view with required params for __init__.
-    attributes = {f'{camel_to_snake(name)}': cls(**init_values.get(cls, {}))
+    attributes = {f'{attributize_string(name)}': cls(**init_values.get(cls, {}))
                   for name, cls in collected_widgets.items()}
 
     # Required for the Tree widgets to function properly.
@@ -97,16 +91,16 @@ def test_widget_init(test_view, widget_name):
     ```
 
     The Button.__init__ seem to be delayed until the view.btn is accessed.
-    We got the view as teh test_view fixture, so we now need to access it to
+    We got the view as the test_view fixture, so we now need to access it to
     check it won't produce an exception. """
 
-    assert getattr(test_view, camel_to_snake(widget_name))
+    assert getattr(test_view, attributize_string(widget_name))
 
 
 @pytest.mark.parametrize('widget_name', collected_widgets.keys())
 def test_widget_stringification(test_view, widget_name):
     """ Tests whether the widget can be stringified.
     All the widgets that can be instantiated should be able to stringify."""
-    wgt = getattr(test_view, camel_to_snake(widget_name))
+    wgt = getattr(test_view, attributize_string(widget_name))
     assert isinstance(str(wgt), str)
     assert isinstance(repr(wgt), str)
