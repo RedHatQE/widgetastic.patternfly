1c65ebcccd60ce35fbc63ef55c9d948e00ef38cc
Mike Shriver <mshriver@redhat.com>
2022-02-21 11:23:21 -0500

    Move to py3.8 minimum
    Move setuptools config into setup.cfg
    Set minimum python version

--- setup.py
+++ setup.py
@@ -1,23 +1,6 @@
-import codecs
-
 from setuptools import setup
 
 
 setup(
-    name="widgetastic.patternfly",
     use_scm_version=True,
-    author="Milan Falesnik",
-    author_email="mfalesni@redhat.com",
-    description="Patternfly widget library for Widgetastic",
-    long_description=codecs.open("README.rst", mode="r", encoding="utf-8").read(),
-    license="Apache license",
-    url="https://github.com/RedHatQE/widgetastic.patternfly",
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Software Development :: Quality Assurance",
-        "Topic :: Software Development :: Testing",
-    ],
 )
